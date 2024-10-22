cb0d62cffdd743b5460b281bf89fb252ec2abf58
sugiyama1692 <kyoei.engine.mba2408.starlight@gmail.com>
2024-10-22 13:53:23 +0900

    Initial Commit

---
+++ OriginalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OriginalApp
+//
+//  Created by mba2408.starlight kyoei.engine on 2024/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
